6619f966119e6ef973ce907467cdf91fa44cf56e
gthornbuckle <gthornbuckle@gmail.com>
2023-04-15 22:14:30 +0100

    Fixes orange block text colour

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -4,7 +4,7 @@ const ShuffleThemes = () =>{
         ["#51ec6d", "#49d462", "#3aaa4e", "#ff3ce7"], //Green
         ["#f92046", "#e01d3f", "#b31732", "#00ffc4"], //Red
         ["#9921f7", "#7a1ac6", "#62159e", "#17ff00"], //Purple
-        ["#ff9404", "#ea8c0e", "#ca780b", "#00d1ff"], //Orange
+        ["#ff7100", "#e16300", "#bf5400", "#3eff40"], //Orange
         ["#f1309c", "#f459b0", "#c3478d", "#00ec98"], //Pink
         ["#f0e247", "#ddd039", "#b7ab1f", "#0077ff"] //Yellow
     ];
