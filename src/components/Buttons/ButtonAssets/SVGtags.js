1822c09793ce06642ade2913d6e0ca6c1f7e1a9f
gthornbuckle <gthornbuckle@gmail.com>
2023-04-18 20:31:46 +0100

    Fixes gear icon transform origin

---
+++ src/components/Buttons/ButtonAssets/SVGtags.js
@@ -0,0 +1,20 @@
+{/* <motion.svg className="absolute" width="24" height="24" viewBox="0 0 16 16"  stroke="#14b8a6" strokeWidth="1" strokeLinecap="flat" fill="none"
+style={{ transformOrigin: "4px 4px" }}
+initial={false}
+animate={isHovered ? "hover" : "initial"}
+variants={gearVariants}>
+    <motion.circle class="cls-1" cx="2.87" cy="3.23" r="1.66"/>
+    <motion.line class="cls-1" x1="2.87" y1="4.89" x2="2.87" y2="5.97"/>
+    <motion.line class="cls-1" x1="1.44" y1="4.06" x2="0.5" y2="4.6"/>
+    <motion.line class="cls-1" x1="1.44" y1="2.41" x2="0.5" y2="1.87"/>
+    <motion.line class="cls-1" x1="2.87" y1="1.58" x2="2.87" y2="0.5"/>
+    <motion.line class="cls-1" x1="4.3" y1="2.4" x2="5.24" y2="1.86"/>
+    <motion.line class="cls-1" x1="4.31" y1="4.06" x2="5.24" y2="4.6"/>
+</motion.svg>
+
+<motion.svg className="absolute" width="15" height="15" viewBox="0 0 10 10" stroke="#14b8a6" strokeWidth="1" strokeLinecap="round" fill="none"
+initial={false}
+animate={isHovered ? "hover" : "initial"}
+variants={plusVariants}>
+    <motion.line class="cls-1" x1="0.5" y1="3.56" x2="6.63" y2="3.56"/><line class="cls-1" x1="3.56" y1="6.63" x2="3.56" y2="0.5"/>
+</motion.svg> */}
--- src/components/Buttons/EditButton.js
+++ src/components/Buttons/EditButton.js
@@ -1,5 +1,6 @@
 import { React, useState } from "react";
 import { motion } from 'framer-motion'
+import gearIcon from './ButtonAssets/gear.svg'
 
 const textHoverColour = {
     initial: {
@@ -37,7 +38,7 @@ const gearVariants ={
         transition: {
                 repeat: Infinity, 
                 repeatType: "loop", 
-                duration: 0.8,
+                duration: 1,
                 ease: "linear"
         }
     }
@@ -48,6 +49,7 @@ function EditButton(){
     
     return (
         <div className="cursor-pointer px-3" onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}>
+            
             <motion.p className="text-teal-500 text-2xl pl-11"
             initial={false}
             animate={isHovered ? "hover" : "initial"}
@@ -63,19 +65,10 @@ function EditButton(){
                     <motion.circle class="cls-1" cx="3.48" cy="13.69" r="2.98"/>
                     <motion.path class="cls-2" d="M6.45,13.69V.5s1.74,4.74,4.74,4.74"/>
                 </motion.svg>
-                <motion.svg className="absolute" width="16" height="16" viewBox="0 0 9 10"  stroke="#14b8a6" strokeWidth="1" strokeLinecap="round" fill="none"
-                style={{ transformOrigin: "5px 5px" }}
+                <motion.img src={gearIcon} alt="gear" className="w-[10px] h-[10px]"
                 initial={false}
                 animate={isHovered ? "hover" : "initial"}
-                variants={gearVariants}>
-                    <motion.circle class="cls-1" cx="2.87" cy="3.23" r="1.66"/>
-                    <motion.line class="cls-1" x1="2.87" y1="4.89" x2="2.87" y2="5.97"/>
-                    <motion.line class="cls-1" x1="1.44" y1="4.06" x2="0.5" y2="4.6"/>
-                    <motion.line class="cls-1" x1="1.44" y1="2.41" x2="0.5" y2="1.87"/>
-                    <motion.line class="cls-1" x1="2.87" y1="1.58" x2="2.87" y2="0.5"/>
-                    <motion.line class="cls-1" x1="4.3" y1="2.4" x2="5.24" y2="1.86"/>
-                    <motion.line class="cls-1" x1="4.31" y1="4.06" x2="5.24" y2="4.6"/>
-                </motion.svg>
+                variants={gearVariants}/>
             </motion.div>
         </div>
     )
